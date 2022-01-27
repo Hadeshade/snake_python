52fc6db56f4877db66e49e46dbd2b7510eefc42a
Marco <marcoantonionemetala@gmail.com>
2022-01-27 09:40:40 -0300

    Movimento Continuo
    	Em vez de  clicar o tempo todo nas setas para andar, agora podemos apenas usar as setas para mudar a direção do personagem, sem que ele pare de andar. Movimento uniforme.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ast import Pass
 import pygame
 from pygame.locals import *
+import time
 
 
 class Snake:
@@ -9,6 +10,7 @@ class Snake:
         self.block = pygame.image.load("resources/block.jpg").convert()
         self.x = 100
         self.y = 100
+        self.direction = 'down'
 
     def draw(self):
         self.parent_screen.fill((110,110,5))
@@ -16,16 +18,24 @@ class Snake:
         pygame.display.flip()
 
     def move_left(self):
-        self.x -= 10
-        self.draw()
-    def move_rigth(self):
-        self.x += 10
-        self.draw()
+        self.direction = 'left'
+    def move_right(self):
+        self.direction = 'right'
     def move_down(self):
-        self.y += 10
-        self.draw()
+        self.direction = 'down'
     def move_up(self):
-        self.y -= 10
+        self.direction = 'up'
+
+    def walk(self):
+        if self.direction == 'left':
+            self.x -= 10
+        if self.direction == 'right':
+            self.x += 10
+        if self.direction == 'down':
+            self.y += 10
+        if self.direction == 'up':
+            self.y -= 10
+
         self.draw()
     
     
@@ -55,11 +65,13 @@ class Game:
                     if event.key == K_LEFT:
                         self.snake.move_left()
                     if event.key == K_RIGHT:
-                        self.snake.move_rigth()
+                        self.snake.move_right()
 
                 elif event.type == QUIT:
                     running = False
 
+            self.snake.walk()
+            time.sleep(0.2)
 
 if __name__ == "__main__":
     game = Game()
