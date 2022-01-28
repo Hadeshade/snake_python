b4388dcd954ad1a7a80e6252f38b3bc944cf9eeb
Marco <marcoantonionemetala@gmail.com>
2022-01-28 11:09:20 -0300

    /feat:Aumento da Cobra e Score
    	Foi implementado a lógica de aumento da cobra quando come a maça, mudar de posição a maça e foi adicionado o Score que nada mais é um contador na tela do tamanho da cobrinha. Foi adicionado alguns comentários também para ficar claro o que cada parte do código faz.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from ctypes.wintypes import SIZE
 import pygame
 from pygame.locals import *
 import time
+import random
 
 SIZE = 40
 
@@ -10,14 +11,19 @@ class Apple:
     def __init__(self, parent_screen):
         self.image = pygame.image.load("resources/apple.jpg").convert()
         self.parent_screen = parent_screen
-        self.x = SIZE*3
-        self.y = SIZE*3
+        self.x = 120
+        self.y = 120
 
     def draw(self):
         self.parent_screen.blit(self.image, (self.x,self.y))
         pygame.display.flip()
 
+    def move(self):
+        self.x = random.randint(0,24)*SIZE
+        self.y = random.randint(0,19)*SIZE
+
 class Snake:
+    #Definições inicias da classe o self é o this, parent_screen é herença de outra classe e lenght parametro;
     def __init__(self, parent_screen, lenght):
         self.lenght = lenght
         self.parent_screen = parent_screen
@@ -26,12 +32,19 @@ class Snake:
         self.y = [SIZE]*lenght
         self.direction = 'down'
 
+    def increase_length(self):
+        self.lenght+=1
+        self.x.append(-1)
+        self.y.append(-1)
+
+    # Desenha todo o tamanho da cobra
     def draw(self):
         self.parent_screen.fill((110,110,5))
         for i in range(self.lenght):
             self.parent_screen.blit(self.block, (self.x[i],self.y[i]))
         pygame.display.flip()
 
+    #Apenas define a direção que deve se movimentar
     def move_left(self):
         self.direction = 'left'
     def move_right(self):
@@ -43,10 +56,12 @@ class Snake:
 
     def walk(self):
 
+        #Coloca a posição do último quadrado no proximo (faz a movimentação)
         for i in range(self.lenght-1,0,-1):
             self.x[i] = self.x[i-1]
             self.y[i] = self.y[i-1]
-
+        
+        #Lógica para movimentar os quadrados (a cobra)
         if self.direction == 'left':
             self.x[0] -= SIZE
         if self.direction == 'right':
@@ -67,15 +82,35 @@ class Game:
         self.surface = pygame.display.set_mode((1000,800))
         self.surface.fill((92,50,84))
         #Cria objeto Snake e desenha ela:
-        self.snake = Snake(self.surface, 5)
+        self.snake = Snake(self.surface, 2)
         self.snake.draw()
         #Cria objeto maça:
         self.apple = Apple(self.surface)
         self.apple.draw()
 
+    def is_collision(self, x1,y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >= y2 and y1 < y2 + SIZE:
+                return True
+        
+        return False
+
+
+
     def play(self):
         self.snake.walk()
         self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
+
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.increase_length()
+            self.apple.move()
+
+    def display_score(self):
+        font = pygame.font.SysFont('arial', 30)
+        score = font.render(f"Score: {self.snake.lenght}", True, (200,200,200))
+        self.surface.blit(score, (800,10))
 
     def run(self):
         #Loop principal
@@ -100,7 +135,7 @@ class Game:
 
             self.play()
 
-            time.sleep(0.3)
+            time.sleep(0.2)
 
 if __name__ == "__main__":
     game = Game()
